ae0e06a3fbacf87d415386659d6eb13516073ea8
palf <jack.palf@gmail.com>
2013-05-16 08:06:38 +0100

    added world js file

---
+++ public/javascripts/paddle_world.js
@@ -0,0 +1,42 @@
+function PaddleWorld() {
+
+  this.addLeftPaddle = function () {
+
+  }
+
+}
+
+
+
+// var left_paddle = {
+//   id: "left_paddle",
+//   x: (10) / SCALE,
+//   y: canvas.center.y,
+//   halfWidth: 5 / SCALE,
+//   halfHeight: 30 / SCALE,
+//   color: 'yellow'
+// }
+
+// var right_paddle = {
+//   id: "right_paddle",
+//   x: (canvasWidth - 10) / SCALE,
+//   y: canvas.center.y,
+//   halfWidth: 5 / SCALE,
+//   halfHeight: 30 / SCALE,
+//   color: 'yellow'
+// }
+
+// var ball = {
+//   id: "ball",
+//   x: canvas.center.x,
+//   y: canvas.center.y,
+//   radius: 0.5,
+//   color: 'red'
+// }
+
+
+// var initialState = [
+//   left_paddle,
+//   right_paddle,
+//   ball
+// ];
